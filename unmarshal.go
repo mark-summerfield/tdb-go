e3705c18848a329e75c28e99a0f87242878625af
mark-summerfield <mark@qtrac.eu>
2022-11-17 09:12:45 +0000

    preparing for unmarshal

--- unmarshal.go
+++ unmarshal.go
@@ -10,9 +10,5 @@ import (
 // Unmarshal reads the data from the given string (as raw UTF-8-encoded
 // bytes) into a (pointer to a) database struct.
 func Unmarshal(data []byte, db any) error {
-	/* NOTE look at encoding/{csv,json,xml} to see how they do it, e.g.,
-	NewReader, NewWriter ?
-	*/
-	// TODO
 	return nil // TODO
 }
