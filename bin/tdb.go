16527fbb14174adab7d7b06991273fbff3200b75
mark-summerfield <mark@qtrac.eu>
2022-11-23 16:47:29 +0000

    nicer cli

--- bin/tdb.go
+++ bin/tdb.go
@@ -17,7 +17,7 @@ func main() {
 func getConfig() config {
 	parser := clip.NewParser()
 	parser.LongDesc = "Converts Tdb or CSV input to CSV, JSON, SQLite " +
-		"Tdb, UXF, or XML.\n\nUse -- before the positionals if either is -."
+		"Tdb, UXF, or XML."
 	parser.PositionalCount = clip.TwoPositionals
 	parser.PositionalHelp = "FILE1 is - for stdin or a .csv, .tdb, or " +
 		".tdb.gz file. FILE2 is - for stdout (in Tdb format), or a " +
@@ -35,4 +35,9 @@ type config struct {
 	decimals int
 	infile   string
 	outfile  string
+}
+
+func (me config) String() string {
+	return fmt.Sprintf("{decimals: %d, infile: %q, outfile: %q}",
+		me.decimals, me.infile, me.outfile)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/mark-summerfield/tdb-go
 go 1.19
 
 require (
-	github.com/mark-summerfield/clip v0.4.7
-	github.com/mark-summerfield/gong v0.6.1
+	github.com/mark-summerfield/clip v0.5.0
+	github.com/mark-summerfield/gong v0.7.0
 	github.com/mark-summerfield/gset v0.8.0
 )
 
