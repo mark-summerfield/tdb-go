81abba99141d363f3ba39e2584766432dc1a80b3
mark-summerfield <mark@qtrac.eu>
2022-11-28 08:53:43 +0000

    more on cli

--- bin/tdb.go
+++ bin/tdb.go
@@ -11,6 +11,8 @@ import (
 
 func main() {
 	config := getConfig()
+	// TODO if config.show use tview to show the tables and the records in
+	// the selected table (see tview postgres eg)
 	fmt.Println(config)
 }
 
@@ -21,19 +23,22 @@ func getConfig() config {
 	parser.PositionalCount = clip.TwoPositionals
 	parser.PositionalHelp = "FILE1 is - for stdin or a .csv, .tdb, or " +
 		".tdb.gz file. FILE2 is - for stdout (in Tdb format), or a " +
-		".csv, .json, .sqlite, .tdb, .tdb.gz, .uxf, .uxf.gz, or .xml file."
+		".csv, .json, .sqlite, .tdb, .tdb.gz, .uxf, .uxf.gz, or .xml "+
+		"file (FILE2 is ignored if -s, --show is used)."
+	showOpt := parser.Flag("show",
+		"Show the given .tdb file in a text user interface.")
 	decimalsOpt := parser.IntInRange("decimals", "How many decimal digits "+
 		"to use. Range 1-19 or 0 (few as possible; the default).", 1, 19, 0)
 	if err := parser.Parse(); err != nil {
 		fmt.Println(err)
 	}
-	return config{decimalsOpt.Value(), parser.Positionals[0],
-		parser.Positionals[1]}
+	return config{showOpt.Value(), decimalsOpt.Value(),
+		parser.Positionals[0], parser.Positionals[1]}
 }
 
 type config struct {
+	show     bool
 	decimals int
 	infile   string
 	outfile  string
 }
-
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/mark-summerfield/tdb-go
 go 1.19
 
 require (
-	github.com/mark-summerfield/clip v0.6.0
+	github.com/mark-summerfield/clip v0.6.1
 	github.com/mark-summerfield/gong v0.7.0
 	github.com/mark-summerfield/gset v0.8.0
 )
